8057ef6677671050eb1d09b3543939e30051e611
yhx189 <yanghu2019@u.northwestern.edu>
2016-04-13 11:20:41 -0500

    quadratic

--- src/quadratic.cpp
+++ src/quadratic.cpp
@@ -5,6 +5,32 @@
 //
 
 int main(){
-    vector<int> parameters;
+    vector<double> parameters;
+    cout << "enter the parameters a,b,c of the quadratic function\n";
+    for(int i=0; i< 3; i++){
+        double tmp;
+        cin >> tmp;
+        parameters.push_back(tmp);
+    }
+    double a = parameters[0];
+    double b = parameters[1];
+    double c = parameters[2];
+    try{
+        if((b*b - 4*a*c) > 0){
+            vector<double> results;
+            results.push_back((-b + sqrt(b*b - 4*a*c))/(2*a));
+            results.push_back((-b - sqrt(b*b - 4*a*c))/(2*a));
+            for(int i = 0; i < results.size(); i++){
+                cout << "results[" << i <<"] is: " << results[i] << endl;
+            }
+        }else if((b*b - 4*a*c) == 0){
+            double res = -b /(a*2);
+            cout << "result is : " << res << endl;
+        }else {
+            throw 7;
+        }
+    } catch(int e){
+        error("error");
+    }
     return 0;
 }
